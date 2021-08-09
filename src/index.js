820c4d0e92bfe40489727e624af596d2d3b10864
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-08-09 11:24:48 -0300

    Primeiros passos dos com node.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+      
+     res.send("Hello Word Express");
+});
+
+app.listen(3333, () => {
+    console.log('Servidor rodando na porta 3333');
+})
